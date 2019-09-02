aa97c53e318b0374d4035fd31253c86ca4fcd7b3
Gopal oswal <gopal@kritin.in>
2019-09-02 23:57:50 +0530

    integrated github api

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -7,7 +7,40 @@ import {Api} from '../api/api';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-username: any;
+  username: any;
+  UserDetail: any = {
+    avatar_url: '',
+    bio: '',
+    blog: '',
+    company: '',
+    created_at: '',
+    email: '',
+    events_url: '',
+    followers: '',
+    followers_url: '',
+    following: '',
+    following_url: '',
+    gists_url: '',
+    gravatar_id: '',
+    hireable: '',
+    html_url: '',
+    id: '',
+    location: '',
+    login: '',
+    name: '',
+    node_id: '',
+    organizations_url: '',
+    public_gists: '',
+    public_repos: '',
+    received_events_url: '',
+    repos_url: '',
+    site_admin: '',
+    starred_url: '',
+    subscriptions_url: '',
+    type: '',
+    updated_at: '',
+    url: '',
+  };
   constructor(private api: Api) {}
 
   /**
@@ -16,6 +49,7 @@ username: any;
   GetUserDetail() {
       this.api.GetUserDetail(this.username).then(res => {
         console.log(res);
+        this.UserDetail = res;
       }).catch(err => {
         console.log(err);
       });
